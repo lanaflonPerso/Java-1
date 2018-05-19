dfe77543756f7400957f06c34cb5dbb6b644645c
vonrepiks <ico_skipernov@abv.bg>
2018-05-19 14:31:46 +0900

    remove Java Web package from this repository

--- Java Web/Java Web Development Basics/Exercises/HTTP-Protocol/src/javache/utility/Helpers.java
+++
@@ -1,9 +0,0 @@
-package javache.utility;
-
-public final class Helpers {
-    private Helpers() {}
-
-    public static String getExtension(String url) {
-        return url.substring(url.lastIndexOf(".") + 1);
-    }
-}
